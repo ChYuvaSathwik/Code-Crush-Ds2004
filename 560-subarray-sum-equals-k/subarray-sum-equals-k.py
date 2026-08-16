bd9f95bb0e1af2fcdeb9ab7b3d8e5888a5176f61
Chinta Yuva Satya Sathwik <sathwikprince2004@gmail.com>
2026-08-16 14:28:41 +0530

    Time: 28 ms (83.35%) | Memory: 21.9 MB (34.41%) - LeetSync

---
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -0,0 +1,16 @@
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        sathwik={}
+        count1=0
+        sathwik[0]=1
+        prefix=0
+        for i in nums:
+            prefix=prefix+i
+            L=prefix-k
+            if L in sathwik:
+                count1=count1+sathwik[L]
+            if prefix in sathwik:
+                sathwik[prefix]=sathwik[prefix]+1
+            else:
+                sathwik[prefix]=1
+        return count1
